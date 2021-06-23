5820a7076b6193573dbcbc18a96bfa38feb2875e
FatmaBakir <fatma.k.bakir@gmail.com>
2021-06-23 08:15:57 -0400

    xxx

---
+++ experimental.py
@@ -0,0 +1,9 @@
+a = 45
+
+if a > 50:
+    print("I am bigger than 50")
+else:
+    print("I am smaller than 50")
+
+for name in ["ALi", "Fatma", "Busra"]:
+    print(f"Welcome to the class, {name}!")
